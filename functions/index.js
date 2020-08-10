4f1ccb7e630d3c7bc93abd1bbeacf2124053a101
Artemas J. Radik <ajradik@gmail.com>
2020-08-10 07:20:19 -0400

    can send messages

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,12 @@
 const functions = require('firebase-functions');
+const messaging = require('firebase-messaging')
 const admin = require('firebase-admin');
 const https = require('https');
 const toString = require('stream-to-string')
 const clone = require('rfdc')();
 admin.initializeApp();
 
-exports.matchingAlgo = functions.https.onCall(async (data, context) => {
-
-  console.log("function called");
+exports.matchStudentToMentors = functions.https.onCall(async (data, context) => {
 
   //File Operations
   const scenariosFromFile = await fetchAndReadAlgorithmMatrix();
@@ -19,83 +18,132 @@ exports.matchingAlgo = functions.https.onCall(async (data, context) => {
     return;
   }
 
-  console.log("check 1");
 
   const usersRef = admin.firestore().collection("users");
-  console.log("check 1.1");
   const studentRef = usersRef.doc(uid);
-  console.log("check 1.2");
   const studentDoc = await studentRef.get();
-
-  const uidToRelativeWeight = []
-
-  console.log("check 2");
-
+  const mentorDocToRelativeWeight = []
   const mentorsQuery = await usersRef.where("accountType", "==", "mentor").get();
 
-  console.log(mentorsQuery.size);
-  console.log("check 4");
-
   for (let mentorDoc of mentorsQuery.docs) {
-    console.log(mentorDoc.id);
-    console.log("check 4.1");
     let scenarios = clone(scenariosFromFile);
-    console.log("check 4.2");
     let relativeWeight = 0;
-    console.log("check 4.3");
-    console.log("check 5");
 
     for (let scenario of scenarios) {
       //Replace Relative Variables
-      console.log(scenario);
-      console.log("check 5.1");
       for (let i = 0; i < scenario.length; i++) {
-        console.log("check 5.2");
         let splitItem = scenario[i].split(" ");
-        console.log("check 5.3");
         if (splitItem[0] == "$MENTOR") {
-          console.log(splitItem);
           scenario[i] = mentorDoc.data()[splitItem[1]];
         }
         else if (splitItem[0] == "$STUDENT") {
           scenario[i] = studentDoc.data()[splitItem[1]];
         }
-        console.log("check 5.4");
       }
 
-      console.log("check 6");
-      console.log(scenario);
-
       let mentorKeys = scenario[0].split(" && ");
-      console.log("check 6.1");
       let mentorValues = scenario[1].split(" && ");
-      console.log("check 6.2");
       let studentKeys = scenario[2].split(" && ");
-      console.log("check 6.3");
       let studentValues = scenario[3].split(" && ");
-      console.log("check 6.4");
-
-      console.log("check 7");
 
       if (passesChecks(mentorKeys, mentorValues, mentorDoc) && passesChecks(studentKeys, studentValues, studentDoc)) {
         relativeWeight += parseInt(scenario[4]);
       }
     }
 
-    console.log("RELATIVE WEIGHT FOR " + mentorDoc.id + " = " + relativeWeight);
-    uidToRelativeWeight.push({uid: mentorDoc.id, relativeWeight: relativeWeight});
+    mentorDocToRelativeWeight.push({mentorDoc: mentorDoc, relativeWeight: relativeWeight});
   }
 
-  console.log("check 8");
+  mentorDocToRelativeWeight.sort((a, b) => (a.relativeWeight < b.relativeWeight) ? 1 : -1);
 
-  uidToRelativeWeight.sort((a, b) => (a.relativeWeight > b.relativeWeight) ? 1 : -1);
+  let highestWeightMentor = mentorDocToRelativeWeight[0];
+  let highestWeightNonWhiteMentor;
+  let highestWeightNonMaleMentor;
 
-  console.log("check 9");
+  for (let entry of mentorDocToRelativeWeight) {
+    if (entry !== highestWeightMentor && entry !== highestWeightNonWhiteMentor && entry !== highestWeightNonMaleMentor) {
+      if (entry.mentorDoc.data().race !== "white") {
+        highestWeightNonWhiteMentor = entry;
+      }
+      else if (entry.mentorDoc.data().gender !== "male") {
+        highestWeightNonMaleMentor = entry;
+      }
+      if (highestWeightNonWhiteMentor !== undefined && highestWeightNonMaleMentor !== undefined) {
+        break;
+      }
+    }
+  }
 
-  let highestWeightMentor = uidToRelativeWeight[uidToRelativeWeight.length-1];
+  if (highestWeightNonWhiteMentor === undefined) {
+    for (let entry of mentorDocToRelativeWeight) {
+      if (entry !== highestWeightMentor && entry !== highestWeightNonWhiteMentor && entry !== highestWeightNonMaleMentor) {
+        highestWeightNonWhiteMentor = entry;
+        break;
+      }
+    }
+  }
 
-  console.log("UID: " + highestWeightMentor.uid + " RELATIVE_WEIGHT: " + highestWeightMentor.relativeWeight)
+  if (highestWeightNonMaleMentor === undefined) {
+    for (let entry of mentorDocToRelativeWeight) {
+      if (entry !== highestWeightMentor && entry !== highestWeightNonWhiteMentor && entry !== highestWeightNonMaleMentor) {
+        highestWeightNonMaleMentor = entry;
+        break;
+      }
+    }
+  }
+
+  if (highestWeightMentor !== undefined) {
+    await match(studentDoc, highestWeightMentor, usersRef);
+  }
+
+  if (highestWeightNonWhiteMentor !== undefined) {
+    await match(studentDoc, highestWeightNonWhiteMentor, usersRef);
+  }
+
+  if (highestWeightNonMaleMentor !== undefined) {
+    await match(studentDoc, highestWeightNonMaleMentor, usersRef);
+  }
+
 });
+
+exports.push = functions.https.onCall(async (data, context) => {
+  // pushNotification("fL6LkZovjEq5lJpGdvnTjy:APA91bGJooLh4rhXLgDxijpNKj9HDFDIkbn4o1KI6-FvUA9Z8AjUrTmrWXhv7iM711T3hc2aTUJ6pWPWpHgfgb90Rl8Xqj-X0eRc_IQFY6epFgRCnwjHY7gfZEur_Cqdrx-TziIxzZ6r", "Test Push Notif", "Test Message Body.").then((result) => {console.log(result)}).catch((error) => {console.log(error)})
+
+  var message = {
+    notification: {
+      title: "You have a new service request",
+      body: "this is the main body"
+    },
+    data: {
+      score: '850',
+      time: '2:45'
+    }
+  };
+
+  return admin.messaging().sendToDevice("cmu32orCrkMmpJoCQNn_Jz:APA91bEU-IsGt4Bwfd1n-7mJ3qzbyEizMBVBeCGz-HaFx6QfBXw2qCgt-MZMsXGIstB3ht1WMsD0usVe0i4KL7FGMs171ekpgeGCtkOrXpQK6pJyP5SFzKhW83jvkFfKE_KeQ94vlgeK", message)
+      .then(function (response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function (error) {
+        console.log("Error sending message:", error);
+      });
+});
+
+async function pushNotification(token, title, body) {
+  const message = {
+    notification: {
+      title: title,
+      body: body
+    }
+  };
+
+  return admin.messaging.sendToDevice(token, message);
+}
+
+async function match(studentDoc, mentorBundle, usersRef) {
+  await usersRef.doc(studentDoc.id).collection("allowList").doc(mentorBundle.mentorDoc.id).set({"matchWeight": mentorBundle.relativeWeight});
+  await usersRef.doc(mentorBundle.mentorDoc.id).collection("allowList").doc(studentDoc.id).set({"matchWeight": mentorBundle.relativeWeight});
+}
 
 function passesChecks(keys, values, document) {
   let checksPassed = 0
